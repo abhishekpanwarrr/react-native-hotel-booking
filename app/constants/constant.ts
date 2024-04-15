e6b4e901f685ea75069b6bc1f52e0a50ed97699f
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2024-04-15 19:04:50 +0530

    complete profile page

--- app/constants/constant.ts
+++ app/constants/constant.ts
@@ -5,3 +5,31 @@ export const COLORS = {
     white: "#fff"
 }
 export const BACKEND_URL = "https://hotel-backend-taupe.vercel.app/api/v1/"
+
+export const USER_PROFILE_ITEMS = [
+    {
+        id: 1,
+        title: "Personal  Info",
+        icon: "person-outline"
+    },
+    {
+        id: 2,
+        title: "Privacy & Sharing",
+        icon: "shield-checkmark-outline"
+    },
+    {
+        id: 3,
+        title: "Notification",
+        icon: "notifications-outline"
+    },
+    {
+        id: 4,
+        title: "Review",
+        icon: "alert-circle-outline"
+    },
+    {
+        id: 5,
+        title: "Logout",
+        icon: "log-out-outline"
+    },
+]
--- app/screens/Profile/index.tsx
+++ app/screens/Profile/index.tsx
@@ -1,11 +1,99 @@
-import {SafeAreaView, Text, View} from 'react-native';
+import {Image, SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
+import {getUserFromLocalStorage} from '../../hooks/useStorage';
+import {useEffect, useState} from 'react';
+import {COLORS, USER_PROFILE_ITEMS} from '../../constants/constant';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-interface ProfileProps {}
+interface User {
+  __v: number;
+  _id: string;
+  address: string;
+  createdAt: string;
+  email: string;
+  fullName: string;
+  likedHotel: [];
+  updatedAt: string;
+}
 
-const Profile = ({}: ProfileProps) => {
+const Profile = () => {
+  const [user, setUser] = useState<User>({} as User);
+
+  useEffect(() => {
+    (async () => {
+      const user = await getUserFromLocalStorage('userInfo');
+      if (user) {
+        setUser(user);
+      }
+    })();
+  }, []);
+
   return (
-    <SafeAreaView>
-      <Text>Profile</Text>
+    <SafeAreaView
+      style={{
+        flex: 1,
+      }}>
+      <View
+        style={{
+          marginTop: 30,
+          marginBottom: 20,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Image
+          source={{
+            uri: 'https://images.pexels.com/photos/1906157/pexels-photo-1906157.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+          }}
+          width={80}
+          height={80}
+          borderRadius={100}
+        />
+        <Text style={{fontSize: 16, fontWeight: '800', marginTop: 10}}>
+          {user?.fullName}
+        </Text>
+        <Text style={{fontSize: 13, fontWeight: '300'}}>{user?.email}</Text>
+      </View>
+      {/* All Links */}
+      {USER_PROFILE_ITEMS?.map(profile => (
+        <TouchableOpacity
+          key={profile.id}
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginBottom: 10,
+            marginTop: 10,
+            borderLeftWidth: 0,
+            borderRightWidth: 0,
+            borderTopWidth: 0,
+            borderBottomWidth: 1,
+            borderColor: '#ddd',
+            paddingBottom: 5,
+            paddingHorizontal: 10,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              gap: 10,
+            }}>
+            <Ionicons name={profile.icon} size={30} color={COLORS.darkBlue} />
+            <Text
+              style={{
+                color: COLORS.darkBlue,
+                fontWeight: '600',
+              }}>
+              {profile.title}
+            </Text>
+          </View>
+          <View>
+            <Ionicons
+              name="chevron-forward-outline"
+              size={20}
+              color={COLORS.darkBlue}
+            />
+          </View>
+        </TouchableOpacity>
+      ))}
     </SafeAreaView>
   );
 };
